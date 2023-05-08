41e92c548c3714f187ea9d81dd9132eae8f81b72
nat5142 <nick.tulli95@gmail.com>
2023-05-07 20:07:38 -0400

    remove dotenv dependency

--- src/kronos/__init__.py
+++ src/kronos/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Nick Tulli"""
 __email__ = "ntulli.dev@gmail.com"
-__version__ = "0.0.12"
+__version__ = "0.0.13"
 
 from .kronos import Kronos, DEFAULT_FORMAT, DEFAULT_TZ, ISO_FMT
 from .utilities import convert_timezone
